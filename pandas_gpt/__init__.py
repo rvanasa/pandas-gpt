854f2a9d0eee1b6139a5110cfbb67dc8f80a12f7
Susman <ryan.susman@fiserv.com>
2023-06-09 10:11:21 -0400

    Added support for 3rd party model proviers for the openai package

--- pandas_gpt/__init__.py
+++ pandas_gpt/__init__.py
@@ -1,32 +1,12 @@
 import pandas as pd
+import sys
+import openai
 
 _ask_cache = {}
-
-class Ask:
-  def __init__(self, *, verbose=False):
-    import os
-    self.verbose = verbose
-
-  @staticmethod
-  def _fill_template(template, **kw):
-    import re
-    from textwrap import dedent
-    result = dedent(template.lstrip('\n').rstrip())
-    for k, v in kw.items():
-      result = result.replace(f'{{{k}}}', v)
-    m = re.match(r'\{[a-zA-Z0-9_]*\}', result)
-    if m:
-      raise Exception(f'Expected variable: {m.group(0)}')
-    return result
-
-  def _get_prompt(self, goal, arg):
-    import io
-    buf = io.StringIO()
-    arg.info(buf=buf)
-    arg_summary = buf.getvalue()
-    arg_name = 'df' if isinstance(arg, pd.DataFrame) else 'data'
-
-    return self._fill_template('''
+model = 'gpt-3.5-turbo'
+deployment_id = None
+engine = None
+template = '''
       Write a Python function `process({arg_name})` which takes the following input value:
 
       {arg_name} = {arg}
@@ -34,52 +14,79 @@ class Ask:
       This is the function's purpose: {goal}
 
       Write the function in a Python code block with all necessary imports and no example usage:
-    ''', arg_name=arg_name, arg=arg_summary.strip(), goal=goal.strip())
+    '''
 
-  def _run_prompt(self, prompt):
-    import openai
-    cache = _ask_cache
-    completion = cache.get(prompt) or openai.ChatCompletion.create(
-      model='gpt-3.5-turbo',
-      messages=[
-        # dict(role='system', content=''),
-        dict(role='user', content=prompt),
-      ]
-    )
-    cache[prompt] = completion
-    return completion['choices'][0]['message']['content']
 
-  def _extract_code_block(self, text):
-    import re
-    pattern = r'```(\s*(py|python)\s*\n)?([\s\S]*?)```'
-    m = re.search(pattern, text)
-    if not m:
-      return text
-    return m.group(3)
+class Ask:
+    def __init__(self, verbose=False):
+        self.verbose = verbose
 
-  def _eval(self, source, *args):
-    _args_ = args
-    scope = dict(_args_=args)
-    exec(self._fill_template('''
-      {source}
-      _result_ = process(*_args_)
-    ''', source=source), scope)
-    return scope['_result_']
+    @staticmethod
+    def _fill_template(template, **kw):
+        import re
+        from textwrap import dedent
+        result = dedent(template.lstrip('\n').rstrip())
+        for k, v in kw.items():
+            result = result.replace(f'{{{k}}}', v)
+        m = re.match(r'\{[a-zA-Z0-9_]*\}', result)
+        if m:
+            raise Exception(f'Expected variable: {m.group(0)}')
+        return result
 
-  def _code(self, goal, arg):
-    prompt = self._get_prompt(goal, arg)
-    result = self._run_prompt(prompt)
-    if self.verbose:
-      print()
-      print(result)
-    return self._extract_code_block(result)
+    def _get_prompt(self, goal, arg):
+        import io
+        buf = io.StringIO()
+        arg.info(buf=buf)
+        arg_summary = buf.getvalue()
+        arg_name = 'df' if isinstance(arg, pd.DataFrame) else 'data'
 
-  def code(self, *args):
-    print(self._code(*args))
+        return self._fill_template(template, arg_name=arg_name, arg=arg_summary.strip(), goal=goal.strip())
 
-  def __call__(self, goal, *args):
-    source = self._code(goal, *args)
-    return self._eval(source, *args)
+    def _run_prompt(self, prompt):
+        cache = _ask_cache
+
+        args = {'model': model, 'deployment_id': deployment_id, 'engine': engine}
+        completion = cache.get(prompt) or openai.ChatCompletion.create(
+            messages=[
+                # dict(role='system', content=''),
+                dict(role='user', content=prompt),
+            ],
+            **args
+        )
+        cache[prompt] = completion
+        return completion['choices'][0]['message']['content']
+
+    def _extract_code_block(self, text):
+        import re
+        pattern = r'```(\s*(py|python)\s*\n)?([\s\S]*?)```'
+        m = re.search(pattern, text)
+        if not m:
+            return text
+        return m.group(3)
+
+    def _eval(self, source, *args):
+        _args_ = args
+        scope = dict(_args_=args)
+        exec(self._fill_template('''
+         {source}
+         _result_ = process(*_args_)
+       ''', source=source), scope)
+        return scope['_result_']
+
+    def _code(self, goal, arg):
+        prompt = self._get_prompt(goal, arg)
+        result = self._run_prompt(prompt)
+        if self.verbose:
+            print()
+            print(result)
+        return self._extract_code_block(result)
+
+    def code(self, *args):
+        print(self._code(*args))
+
+    def __call__(self, goal, *args):
+        source = self._code(goal, *args)
+        return self._eval(source, *args)
 
 
 @pd.api.extensions.register_dataframe_accessor('ask')
@@ -93,10 +100,10 @@ class AskAccessor:
         pass
 
     def _ask(self, **kw):
-      return Ask(**kw)
+        return Ask(**kw)
 
     def _data(self):
-      return self._obj.copy() # TODO: possibly `deep=False`
+        return self._obj.copy()  # TODO: possibly `deep=False`
 
     def __call__(self, goal, *args, **kw):
         ask = self._ask(**kw)
